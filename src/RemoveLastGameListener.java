7af8f1da9e92364162333ace1640a881e1bdd8ce
Thomas Stieglmaier <thomas@stieglmaier.me>
2015-07-04 14:52:26 +0200

    comments + gitignore

--- src/RemoveLastGameListener.java
+++ src/RemoveLastGameListener.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
  * Dummy class such that we can have a joinpoint in another aspect here, too
  */
 public class RemoveLastGameListener implements ActionListener {
+
+    /** filled with pointcut from RemoveLastGame.aj*/
     @Override
     public void actionPerformed(ActionEvent e) {}
 }
